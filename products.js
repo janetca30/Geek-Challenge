9409f710785337ce9fd0b555bc9762ad97f08e79
janetcalderon <janetca30@gmail.com>
2024-05-03 22:35:19 -0300

    :hammer_and_pick: Adding images and giving styles.

---
+++ products.js
@@ -0,0 +1,46 @@
+const electronicProducts = {
+  'electro': [
+    {
+      '_id': 1,
+      'electroImage': './images/products/airpod.jpeg',
+      'electroName': 'XIAOMI headphones',
+      'electroPrice': 8760.00,
+      'electroDetails': 'Xiaomi Mijia Bluetooth 5.3 Wireless Headphones Noise Reduction HiFI Stereo Sound AP09',
+    },
+    {
+      '_id': 2,
+      'electroImage': './images/products/belt.jpeg',
+      'electroName': 'VATLTY tactical belt',
+      'electroPrice': 6985.00,
+      'electroDetails': 'VATLTY New Mens Hard Tactical Belt, Metal Automatic Buckle, IPSC 1100D Gun Belt Nylon Military Belt Male Outdoor Sports Girdle',
+    },
+    {
+      '_id': 3,
+      'electroImage': './images/products/clock.jpeg',
+      'electroName': 'Smartwatch clock',
+      'electroPrice': 37115.00,
+      'electroDetails': '2024 New NFC Smart Watch Men AMOLED Screen Always Time Display PK Ultra Series HK 8 Pro max Waterproof Smartwatch',
+    },
+    {
+      '_id': 4,
+      'electroImage': './images/products/drone.jpeg',
+      'electroName': 'Dron',
+      'electroPrice': 11080.00,
+      'electroDetails': 'E88 Drone Professional 4K HD Dual Camera LED Lighting Aerial Photography Omnidirectional Foldable RC FPV Toys Helicopter New',
+    },
+    {
+      '_id': 5,
+      'electroImage': './images/products/mic.jpeg',
+      'electroName': 'BAIFEILI mic',
+      'electroPrice': 58990.00,
+      'electroDetails': 'BAIFEILI C414 XLR Professional Condenser Microphone 25mm Diaphragm Cardioid Microphone for Broadcasting Podcasting Recording ASMR',
+    },
+    {
+      '_id': 6,
+      'electroImage': './images/products/print.jpeg',
+      'electroName': ' Printer mini thermal',
+      'electroPrice': 12290.00,
+      'electroDetails': 'Mini Portable 2 Inch Thermal Printer Pocket Receipt Printer Wireless Bluetooth 58mm for Android IOS Windows Maker',
+    }
+  ]
+}
